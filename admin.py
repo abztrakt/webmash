a77aeb893440a48fa2b33d88df378592ef149f3f
Craig M. Stimmel <cstimmel@uw.edu>
2011-05-12 17:38:22 -0700

    * Moved 'Text' model to 'LocalText' because it was conflicting with django's own Text class.  * Added the ability to manage LocalTexts to the admin interface.

---
+++ admin.py
@@ -0,0 +1,4 @@
+from webmash.models import LocalText
+from django.contrib import admin
+
+admin.site.register(LocalText)
--- models.py
+++ models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Artifact(models.Model):
+    """ Base class for digital artifacts of all kinds.
+    """
+    pass
+
+class LocalText(Artifact):
+    """ A text artifact stored locally.
+    """
+    text = models.TextField()
