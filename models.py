52f7d365d19a1c96375b77df3a1f44506eeb964b
Craig M. Stimmel <cstimmel@uw.edu>
2011-05-17 17:31:58 -0700

    Added a many-to-many relation to identify parent Artifacts. We should still be able to find parent objects through the child without storing that information in the child object.

--- models.py
+++ models.py
@@ -3,7 +3,7 @@ from django.db import models
 class Artifact(models.Model):
     """ Base class for digital artifacts of all kinds.
     """
-    pass
+    children = models.ManyToManyField("self", blank=True)
 
 class LocalText(Artifact):
     """ A text artifact stored locally.
