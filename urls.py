b57e40ca30cba98bdf1a4a5bff8597e6010ce28e
Craig M. Stimmel <cstimmel@uw.edu>
2011-05-26 23:48:09 -0400

    Added a view that simply lists out all Artifacts and Collections.

--- urls.py
+++ urls.py
@@ -1,5 +1,6 @@
-from django.conf.urls.defaults import *
+from django.conf.urls.defaults import patterns, include, url
 
 urlpatterns = patterns('webmash.views',
-    (r'^$', 'index'),
+    url(r'^objects/$', 'all_objects'),
+    url(r'^$', 'index'),
 )
--- views.py
+++ views.py
@@ -1,4 +1,9 @@
 from django.shortcuts import render_to_response
+from webmash.models import *
 
 def index(request):
     return render_to_response('index.html')
+
+def all_objects(request):
+    all = Base.objects.all()
+    return render_to_response('all_objects.html', {'all':all,})
