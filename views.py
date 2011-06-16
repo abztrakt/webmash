4d51bb4182ecab76fb05a33cbc0111d500ba779c
Craig M. Stimmel <cstimmel@uw.edu>
2011-06-15 17:37:19 -0700

    Added 'Artifacts' and 'Containers' views that simpy return their respective objects. Mostly this will probably just get used for debugging.

--- views.py
+++ views.py
@@ -11,3 +11,11 @@ def all_objects(request):
 def object(request, object_slug):
     object = Base.objects.get(slug=object_slug)
     return render_to_response('object.html', {'object':object,})
+
+def containers(request):
+    containers = Container.objects.all()
+    return render_to_response('all_objects.html', {'all':containers,})
+
+def artifacts(request):
+    artifacts = Artifact.objects.all()
+    return render_to_response('all_objects.html', {'all':artifacts,})
