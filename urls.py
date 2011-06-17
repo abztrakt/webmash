5beff012b34cdd86dc68e8347f0412d240d8f9ae
Craig M. Stimmel <cstimmel@uw.edu>
2011-06-16 17:41:13 -0700

    Added scaffolding for a single container view. At this point it simply mimics the single object view.

--- urls.py
+++ urls.py
@@ -3,6 +3,7 @@ from django.conf.urls.defaults import patterns, include, url
 urlpatterns = patterns('webmash.views',
     url(r'^object/(?P<object_slug>\w+)', 'object'),
     url(r'^objects/$', 'all_objects'),
+    url(r'^container/(?P<object_slug>\w+)', 'container'),
     url(r'^containers/$', 'containers'),
     url(r'^artifacts/$', 'artifacts'),
     url(r'^$', 'index'),
--- views.py
+++ views.py
@@ -16,6 +16,10 @@ def containers(request):
     containers = Container.objects.all()
     return render_to_response('all_objects.html', {'all':containers,})
 
+def container(request, object_slug):
+    object = Base.objects.get(slug=object_slug)
+    return render_to_response('container.html', {'object':object,})
+
 def artifacts(request):
     artifacts = Artifact.objects.all()
     return render_to_response('all_objects.html', {'all':artifacts,})
