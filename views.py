8985250e0003f62372ad41718234285003d6e39a
Craig M. Stimmel <cstimmel@uw.edu>
2011-06-24 16:53:27 -0700

    Added comments so other devs at least have a hope of figuring out what each of the views does.

--- views.py
+++ views.py
@@ -4,24 +4,36 @@ from webmash.models import *
 installed_types = ['Page', 'Folder', 'LocalText',]
 
 def index(request):
+    """ Renders the index page.
+    """
     return render_to_response('index.html')
 
 def all_objects(request):
+    """ Renders a list of all objects in the system.
+    """
     all = Base.objects.all()
     return render_to_response('all_objects.html', {'all':all,})
 
 def object(request, object_slug):
+    """ Renders any generic object.
+    """
     object = Base.objects.get(slug=object_slug)
     return render_to_response('object.html', {'object':object,})
 
 def containers(request):
+    """ Renders a list of alli container type objects.
+    """
     containers = Container.objects.all()
     return render_to_response('all_objects.html', {'all':containers,})
 
 def container(request, object_slug):
+    """ Renders any generic object.
+    """
     object = Base.objects.get(slug=object_slug)
     return render_to_response('container.html', {'object':object, 'types':installed_types,})
 
 def artifacts(request):
+    """ Renders a list of all artifact type objects.
+    """
     artifacts = Artifact.objects.all()
     return render_to_response('all_objects.html', {'all':artifacts,})
