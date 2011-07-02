0397a7bdac4c5680b106b7c788e7e0bc20d2cacd
Craig M. Stimmel <cstimmel@uw.edu>
2011-07-01 17:55:34 -0700

    Added prepopulated slug fields to the Page and Folder models as well.

--- admin.py
+++ admin.py
@@ -1,8 +1,13 @@
 from webmash.models import Page, Folder, LocalText
 from django.contrib import admin
 
-admin.site.register(Folder)
-admin.site.register(Page)
+class FolderAdmin(admin.ModelAdmin):
+    prepopulated_fields = {"slug": ("title",)}
+admin.site.register(Folder, FolderAdmin)
+
+class PageAdmin(admin.ModelAdmin):
+    prepopulated_fields = {"slug": ("title",)}
+admin.site.register(Page, PageAdmin)
 
 class LocalTextAdmin(admin.ModelAdmin):
     prepopulated_fields = {"slug": ("title",)}
